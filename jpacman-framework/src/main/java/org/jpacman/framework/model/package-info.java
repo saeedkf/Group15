ef54133bb3526c7e8de350500b21e5e834a0f013
saeedkf <saeed_karimi@yahoo.com>
2015-01-18 14:17:02 -0800

    Revert "Exercise 1.1"
    This reverts commit a0850b109eb34d8f2cdfddea79c337485a100ccf.

--- jpacman-framework/src/main/java/org/jpacman/framework/model/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
-  * This package contains the pacman model: i.e., it contains representations
-  * for tiles, sprites, the player, the board, the game, etc.
-  * The top level class is Game.
-  *
-  * @author Arie van Deursen, January 2012.
-  */
-package org.jpacman.framework.model;
